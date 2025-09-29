cbfe0e2b0f888fc7cdac8853285c5397779b3f5a
farjad1205 <farjad1205@gmail.com>
2025-09-28 23:43:19 -0500

    fixed asserts and finished with name

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ public:
     virtual ~Shape() {}
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
+    virtual string getName() const = 0;
 };
 
 class Circle : public Shape
@@ -20,7 +21,9 @@ public:
     Circle(double radius) : radius(radius) {}
     double getArea() const override { return M_PI * radius * radius; }
     double getPerimeter() const override { return 2 * M_PI * radius; }
+    string getName() const override { return "Circle"; }
 };
+
 class Rectangle : public Shape
 {
 private:
@@ -30,8 +33,10 @@ private:
 public:
     Rectangle(double length, double width) : length(length), width(width) {}
     double getArea() const override { return length * width; }
-    double getPerimeter() const override { return (2 * length) + (2 * width); }
+    double getPerimeter() const override { return 2 * (length + width); }
+    string getName() const override { return "Rectangle"; }
 };
+
 class RightTriangle : public Shape
 {
 private:
@@ -46,6 +51,7 @@ public:
         double thirdSide = sqrt(base * base + height * height);
         return base + height + thirdSide;
     }
+    string getName() const override { return "Right Triangle"; }
 };
 
 class Square : public Rectangle
@@ -54,7 +60,8 @@ private:
     double length;
 
 public:
-    Square(double length) : Rectangle(length, length) {}
+    Square(double length) : Rectangle(length, length), length(length) {}
+    string getName() const override { return "Square"; }
 };
 
 class IsoTriangle : public RightTriangle
@@ -63,13 +70,14 @@ private:
     double length;
 
 public:
-    IsoTriangle(double length) : RightTriangle(length, length) {}
+    IsoTriangle(double length) : RightTriangle(length, length), length(length) {}
+    string getName() const override { return "IsoTriangle"; }
 };
 
-void printToScreen(Shape *s)
+void printAreaToScreen(Shape *s)
 {
-    cout << "This shape is " << "[name]" << endl;
-    cout << "With an area of " << s->getArea();
+    cout << "This shape is a " << s->getName()
+         << " with an area of " << s->getArea() << endl;
 }
 
 int main()
@@ -80,18 +88,34 @@ int main()
     Shape *sqr = new Square(6);
     Shape *iso = new IsoTriangle(4);
 
-    assert(rect->getArea());
-    assert(rect->getPerimeter());
-    assert(circ->getArea());
-    assert(circ->getPerimeter());
-    assert(tri->getArea());
-    assert(tri->getPerimeter());
-    assert(sqr->getArea());
-    assert(sqr->getPerimeter());
-    assert(iso->getArea());
-    assert(iso->getPerimeter());
-    cout << "all good" << endl;
+    assert(rect->getArea() == 42);
+    assert(rect->getPerimeter() == 26);
 
-    printToScreen(sqr);
+    assert(circ->getArea() == M_PI * 16);
+    assert(circ->getPerimeter() == 2 * M_PI * 4);
+
+    assert(tri->getArea() == 7.5);
+    assert(tri->getPerimeter() == 3 + 5 + sqrt(34));
+
+    assert(sqr->getArea() == 36);
+    assert(sqr->getPerimeter() == 24);
+
+    assert(iso->getArea() == 8);
+    assert(iso->getPerimeter() == 4 + 4 + sqrt(32));
+
+    cout << "All tests passed!" << endl;
+
+    printAreaToScreen(rect);
+    printAreaToScreen(circ);
+    printAreaToScreen(tri);
+    printAreaToScreen(sqr);
+    printAreaToScreen(iso);
+
+    delete rect;
+    delete circ;
+    delete tri;
+    delete sqr;
+    delete iso;
+
     return 0;
 }
